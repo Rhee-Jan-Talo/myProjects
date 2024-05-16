d207d150a447c525773abc158262f8fc720cf352
Rhee-Jan-Talo <jarheetalo@gmail.com>
2024-05-17 00:02:52 +0800

    clean up

--- Dental Clinic Admin System/static/javascript/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('logout_button').addEventListener('click', function(){
-    alert('You have been logout');
-});
-document.querySelector("login_box").addEventListener("click", function(x){
-    const tgt = x.target;
-    if(tgt.classList.contains("login1")){
-        
-    }
-});
